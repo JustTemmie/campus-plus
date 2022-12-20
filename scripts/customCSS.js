708afbe0310f11e786ec7d6902423d09e494ecc4
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-12-20 14:43:27 +0100

    min width

--- scripts/customCSS.js
+++ scripts/customCSS.js
@@ -2,6 +2,7 @@ function resizeCSS() {
     const element = document.getElementById("activityRoot");
     element.style.width = "max-content";
     element.style.maxWidth = "90vw";
+    element.style.minWidth = "500px";
 }
 
 async function checkSettings() {
